50c78798598d43527fe695f73d48b739be374920
Yongran <maiyongran@gmail.com>
2022-08-15 15:14:43 +0100

    create arraylists lab

---
+++ start_code_lab_arraylists_loops/src/main/java/Runner.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+//        SCOTTISH ISLANDS
+        List<String> scottishIslands = new ArrayList<>();
+        scottishIslands.add("Jura");
+        scottishIslands.add("Mull");
+        scottishIslands.add("Skye");
+        scottishIslands.add("Arran");
+        scottishIslands.add("Tresco");
+
+//        1. Add "Coll" to the end of the list
+//        2. Add "Tiree" to the start of the list
+//        3. Add "Islay" after "Jura" and before "Mull"
+//        4. Print out the index position of "Skye"
+//        5. Remove "Tresco" from the list by name
+//        6. Remove "Arran" from the list by index
+//        7. Print the number of islands in your arraylist
+//        8. Sort the list alphabetically
+//        9. Print out all the islands using a for loop
+
+        System.out.println(scottishIslands);
+
+//        NUMBERS
+        List<Integer> numbers = new ArrayList<>();
+        Collections.addAll(numbers, 1, 1, 4, 2, 7, 1, 6, 15, 13, 99, 7);
+
+        System.out.println("numbers: " + numbers);
+
+//        1. Print out a list of the even integers
+//        2. Print the difference between the largest and smallest value
+//        3. Print True if the list contains a 1 next to a 1 somewhere.
+//        4. Print the sum of the numbers,
+//        5. Print the sum of the numbers...
+//           ...except the number 13 is unlucky, so it does not count...
+//           ...and numbers that come immediately after a 13 also do not count.
+//
+//          So [7, 13, 2] would have sum of 9.
+
+    }
+
+}
